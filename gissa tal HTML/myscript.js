ce92f139cf3a56167a4797ed19bec26d9b1cb06a
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2021-10-20 22:28:33 +0200

    Add files via upload

---
+++ gissa tal HTML/myscript.js
@@ -0,0 +1,20 @@
+var slumpran = Math.floor(Math.random() * 100) + 1;
+var val = document.getElementById("gissning");
+var elementtxt1 = document.getElementById("txt1");
+var count = 0;
+
+function slump(){
+    count ++;
+
+        if(val.value < slumpran){
+            elementtxt1.innerHTML="Too low";
+            
+        }
+        else if(val.value > slumpran){
+            elementtxt1.innerHTML="Too high";
+        }
+        else{
+            elementtxt1.innerHTML="YES!";
+        }
+        document.getElementById("txtcount").innerHTML="Count: " + count;
+}
